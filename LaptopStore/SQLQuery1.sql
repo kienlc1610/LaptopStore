358aef3b61aa67b5d2b8d072cbeb95eb6b447055
tuananhnguyen28 <33001181+tuananhnguyen28@users.noreply.github.com>
2017-11-15 20:47:25 +0700

    update sql file

--- LaptopStore/SQLQuery1.sql
+++ LaptopStore/SQLQuery1.sql
@@ -70,31 +70,8 @@ GO
 
 INSERT INTO [Product] (CateID, Name, Price, Image, Description, Quantity, Discount, Status)
 VALUES ('2', 'ASUS-1','15000000', 'asus1.jpg', 'Laptop ASUS-1', '10', '0.1', '1'),
-		('2', 'ASUS-2','14000000', 'asus2.jpg', 'Laptop ASUS-2', '15', '0.05', '0')
-GO
-
-INSERT INTO [Customer] (CustomerID, Password,Fullname, Address, Activated)
-VALUES ('CUS001', 'abc123','Le Thi Xuan', '119/34A Tran Hoan Street District 3 HCMC', '1'),
-		('CUS002', 'abc456','Tran Hung Toan', '34Bis Nguyen Van Loi Street District HCMC', '0')
-GO
-
-INSERT INTO[Order] (CustomerID, Address, Amount, RequireDate, OrderDate, Description, Status)
-VALUES ('CUS002', '34Bis Nguyen Van Loi Street District 1 HCMC', '2', '03/25/2017', '03/27/2017', '', '1'),
-		('CUS001', '119/34A Tran Hoan Street District 3 HCMC', '3', '04/14/2017', '04/16/2017', '', '0')
-GO  
-
-INSERT INTO[OrderDetail] (OrderID, ProductID, Discount, Quantity, UnitPrice)
-VALUES ('1', '2', '0.1', '10', '14500000'),
-		('2', '1', '0.05', '15', '13800000')
-GO
-
---
-
-USE [LapTopStore]
-GO
-
-INSERT INTO [Product] (CateID, Name, Price, Image, Description, Quantity, Discount, Status)
-VALUES ('1', 'DELL-1','13600000', 'dell1.jpg', 'Laptop DELL-1', '10', '0.05', '1'),
+		('2', 'ASUS-2','14000000', 'asus2.jpg', 'Laptop ASUS-2', '15', '0.05', '0'),
+		('1', 'DELL-1','13600000', 'dell1.jpg', 'Laptop DELL-1', '10', '0.05', '1'),
 		('1', 'DELL-2','12300000', 'dell2.jpg', 'Laptop DELL-2', '21', '0.05', '0'),
 		('3', 'SONY-1','17600000', 'sony1.jpg', 'Laptop SONY-1', '22', '0.02', '1'),
 		('3', 'SONY-2','1930000', 'sony2.jpg', 'Laptop SONY-2', '30', '0.02', '1'),
@@ -107,19 +84,25 @@ VALUES ('1', 'DELL-1','13600000', 'dell1.jpg', 'Laptop DELL-1', '10', '0.05', '1
 GO
 
 INSERT INTO [Customer] (CustomerID, Password,Fullname, Address, Activated)
-VALUES ('CUS003', 'abc789','Le Huy', '18/8H Tran Ke Xuong Street Phu Nhuan District HCMC', '1'),
+VALUES ('CUS001', 'abc123','Le Thi Xuan', '119/34A Tran Hoan Street District 3 HCMC', '1'),
+		('CUS002', 'abc456','Tran Hung Toan', '34Bis Nguyen Van Loi Street District HCMC', '0'),
+		('CUS003', 'abc789','Le Huy', '18/8H Tran Ke Xuong Street Phu Nhuan District HCMC', '1'),
 		('CUS004', 'def123','Dang Hoang Nhan', '45/9 Le Loi Street District 1 HCMC', '0'),
 		('CUS005', 'def45', 'Le Viet Khanh', '23B Nguyen Van Nghi Street Go Vap District HCMC', '1')
 GO
 
 INSERT INTO[Order] (CustomerID, Address, Amount, RequireDate, OrderDate, Description, Status)
-VALUES ('CUS005', '23B Nguyen Van Nghi Street Go Vap District HCMC', '5', '09/25/2017', '09/30/2017', '', '1'),
+VALUES ('CUS002', '34Bis Nguyen Van Loi Street District 1 HCMC', '2', '03/25/2017', '03/27/2017', '', '1'),
+		('CUS001', '119/34A Tran Hoan Street District 3 HCMC', '3', '04/14/2017', '04/16/2017', '', '0'),
+		('CUS005', '23B Nguyen Van Nghi Street Go Vap District HCMC', '5', '09/25/2017', '09/30/2017', '', '1'),
 		('CUS003', '18/8H Tran Ke Xuong Street Phu Nhuan District HCMC', '10', '10/14/2017', '10/16/2017', '', '1'),
 		('CUS004', '45/9 Le Loi Street District 1 HCMC', '8', '04/01/2017', '04/04/2017', '', '0')
 GO  
 
 INSERT INTO[OrderDetail] (OrderID, ProductID, Discount, Quantity, UnitPrice)
-VALUES ('5', '3', '0.05', '8', '13200000'),
+VALUES ('1', '2', '0.1', '10', '14500000'),
+		('2', '1', '0.05', '15', '13800000'),
+		('5', '3', '0.05', '8', '13200000'),
 		('4', '4', '0.05', '10', '12000000'),
 		('3', '5', '0.02', '12', '17100000')
 GO
