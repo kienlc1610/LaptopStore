9a69ffd2182d9a3fb93bd112087a092b7d64240b
Unknown <kienlc1610@gmail.com>
2017-11-17 21:29:33 +0700

    category

---
+++ LaptopStore/WebCore/wwwroot/ClientScrpits/Controller/admin-category.controller.js
@@ -0,0 +1,43 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('AdminApp')
+        .controller('AdminCategoryController', AdminProductController);
+
+    AdminProductController.$inject = ['$location', 'AdminService', 'toastr', '$scope'];
+
+    function AdminProductController($location, AdminService, toastr, $scope) {
+        /* jshint validthis:true */
+        var vm = $scope;
+
+        vm.deleteProduct = deleteProduct;
+
+        activate();
+
+        function activate() {
+            getAllCategory();
+        }
+
+        function getAllCategory() {
+            AdminService.getAllCategories()
+                .then(function (category) {
+                    vm.categories = category;
+                })
+                .catch(function (err) {
+                    toastr.error('Error:' + JSON.stringify(err));
+                });
+        }
+
+        function deleteProduct(id) {
+            AdminService.deleteProduct(id)
+                .then(function (products) {
+                    toastr.success("Product" + id + "is deleted!");
+                    activate();
+                })
+                .catch(function (err) {
+                    toastr.error('Error:' + JSON.stringify(err));
+                });
+        }
+    }
+})();
