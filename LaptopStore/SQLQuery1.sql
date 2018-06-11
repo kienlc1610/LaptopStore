5c71d109bb6d4510b4f635911b55cd0dc49d63c2
Ngoc Minh <minh.hn.fm270795@gmail.com>
2018-06-11 20:59:40 +0700

    Thay doi Database

--- LaptopStore/SQLQuery1.sql
+++ LaptopStore/SQLQuery1.sql
@@ -4,72 +4,72 @@ GO
 USE [LapTopStore]
 GO
 
-CREATE TABLE [Category] (
-    [CateID] int NOT NULL IDENTITY(1,1),
-	[Name] nvarchar(50) NOT NULL,
-	[Alias] text NULL,
-	CONSTRAINT [PK_Category] PRIMARY KEY ([CateID])
+CREATE TABLE [LoaiSanPham] (
+    [ID] int NOT NULL IDENTITY(1,1),
+	[Ten] nvarchar(50) NOT NULL,
+	[MoTa] text NULL,
+	CONSTRAINT [PK_Loai] PRIMARY KEY ([ID])
 );
 GO
 
-CREATE TABLE [Product] (
-    [ProductID] int NOT NULL IDENTITY(1,1),
-	[CateID] int NOT NULL,
-    [Name] nvarchar(50) NOT NULL,
-	[Price] float NOT NULL,
-	[Image] nvarchar(max) NOT NULL,
-	[Description] nvarchar(2000) NULL,
-	[Quantity] int NOT NULL,
-	[Discount] float NOT NULL,
-	[Status] bit NOT NULL,
-    CONSTRAINT [PK_Product] PRIMARY KEY ([ProductID]),
-	CONSTRAINT [FK_Product_Category_CateID] FOREIGN KEY ([CateID]) REFERENCES [Category] ([CateID]) ON DELETE CASCADE
+CREATE TABLE [SanPham] (
+    [ID] int NOT NULL IDENTITY(1,1),
+	[IDLoai] int NOT NULL,
+    [Ten] nvarchar(50) NOT NULL,
+	[Gia] float NOT NULL,
+	[Hinh] nvarchar(max) NOT NULL,
+	[MoTa] nvarchar(2000) NULL,
+	[SoLuong] int NOT NULL,
+	[GiamGia] float NOT NULL,
+	[TrangThai] bit NOT NULL,
+    CONSTRAINT [PK_SanPham] PRIMARY KEY ([ID]),
+	CONSTRAINT [FK_Product_Loai] FOREIGN KEY ([IDLoai]) REFERENCES [LoaiSanPham] ([ID]) ON DELETE CASCADE
 );
 GO
 
-CREATE TABLE [Customer] (
-	[CustomerID] nvarchar(100) NOT NULL,
+CREATE TABLE [KhachHang] (
+	[ID] nvarchar(100) NOT NULL,
 	[Email] nvarchar(50) NOT NULL,
-	[Password] nvarchar(20) NOT NULL,
-	[Fullname] nvarchar(max) NOT NULL,
-	[Address] nvarchar(1000) NULL,
-	[Activated] bit NOT NULL,
-	CONSTRAINT [PK_Customer] PRIMARY KEY ([CustomerID])
+	[MatKhau] nvarchar(20) NOT NULL,
+	[HoTen] nvarchar(max) NOT NULL,
+	[DiaChi] nvarchar(1000) NULL,
+	[TrangThai] bit NOT NULL,
+	CONSTRAINT [PK_KhachHang] PRIMARY KEY ([ID])
 );
 GO
 
-CREATE TABLE [Order] (
-	[OrderID] int NOT NULL IDENTITY(1,1),
-	[CustomerID] nvarchar(100) NOT NULL,
-	[Address] nvarchar(1000) NOT NULL,
-	[Amount] float NOT NULL,
-	[RequireDate] datetime NOT NULL,
-	[OrderDate] datetime NOT NULL,
-	[Description] nvarchar(max) NULL,
-	[Status] bit NOT NULL,
-	CONSTRAINT [PK_Order] PRIMARY KEY ([OrderID]),
-	CONSTRAINT [FK_Order_Customer_CustomerID] FOREIGN KEY ([CustomerID]) REFERENCES [Customer] ([CustomerID]) ON DELETE CASCADE
+CREATE TABLE [DonHang] (
+	[ID] int NOT NULL IDENTITY(1,1),
+	[IDKhachHang] nvarchar(100) NOT NULL,
+	[DiaChi] nvarchar(1000) NOT NULL,
+	[SoLuong] float NOT NULL,
+	[NgayGiao] datetime NOT NULL,
+	[NgayDat] datetime NOT NULL,
+	[MoTa] nvarchar(max) NULL,
+	[TrangThai] bit NOT NULL,
+	CONSTRAINT [PK_DonHang] PRIMARY KEY ([ID]),
+	CONSTRAINT [FK_DonHang_KhachHang_IDKhachHang] FOREIGN KEY ([IDKhachHang]) REFERENCES [KhachHang] ([ID]) ON DELETE CASCADE
 );
 GO
 
-CREATE TABLE [OrderDetail] (
+CREATE TABLE [ChiTietDonHang] (
 	[Id] int NOT NULL IDENTITY(1,1),
-	[OrderID] int NOT NULL,
-	[ProductID] int NOT NULL,
-	[Discount] float NOT NULL,
-	[Quantity] int NOT NULL,
-	[UnitPrice] float NOT NULL,
-    CONSTRAINT [PK_OrderDetail] PRIMARY KEY ([Id]),
-    CONSTRAINT [FK_OrderDetail_Order_OrderID] FOREIGN KEY ([OrderID]) REFERENCES [Order] ([OrderID]) ON DELETE CASCADE,
-	CONSTRAINT [FK_OrderDetail_Product_ProductID] FOREIGN KEY ([ProductID]) REFERENCES [Product] ([ProductID]) ON DELETE CASCADE
+	[IDDonHang] int NOT NULL,
+	[IDSanPham] int NOT NULL,
+	[GiamGia] float NOT NULL,
+	[SoLuong] int NOT NULL,
+	[DonGia] float NOT NULL,
+    CONSTRAINT [PK_ChiTietDonHang] PRIMARY KEY ([Id]),
+    CONSTRAINT [FK_ChiTietDonHang_DonHang_IDDonHang] FOREIGN KEY ([IDDonHang]) REFERENCES [DonHang] ([ID]) ON DELETE CASCADE,
+	CONSTRAINT [FK_ChiTietDonHang_Product_ProductID] FOREIGN KEY ([IDSanPham]) REFERENCES [SanPham] ([ID]) ON DELETE CASCADE
 );
 GO
 
-INSERT INTO [Category] (Name, Alias) 
+INSERT INTO [LoaiSanPham] (Ten, MoTa) 
 VALUES ('DELL','dell'), ('ASUS','asus'), ('SONY','sony'), ('ACER','acer'), ('HP','hp')
 GO
 
-INSERT INTO [Product] (CateID, Name, Price, Image, Description, Quantity, Discount, Status)
+INSERT INTO SanPham(IDLoai, Ten, Gia, Hinh, MoTa, SoLuong, GiamGia, TrangThai)
 VALUES ('2', 'Asus Vivobook 14 X405UA-BV327T','15000000', 'asus1.jpg', 'Laptop ASUS-1', '10', '0.1', '1'),
 		('2', 'Asus ZenBook UX430UA-GV261T','14000000', 'asus2.jpg', 'Laptop ASUS-2', '15', '0.05', '0'),
 		('1', 'Dell Vostro 7570-70138565','13600000', 'dell1.jpg', 'Laptop DELL-1', '10', '0.05', '1'),
@@ -84,7 +84,7 @@ VALUES ('2', 'Asus Vivobook 14 X405UA-BV327T','15000000', 'asus1.jpg', 'Laptop A
 		('1', 'Dell Inspiron 5379-JYN0N1','15670000', 'dell4.jpg', 'Laptop DELL-4', '18', '0.05', '1')
 GO
 
-INSERT INTO [Customer] (CustomerID, Email, Password, Fullname, Address, Activated)
+INSERT INTO [KhachHang] (ID, Email, MatKhau, HoTen, DiaChi, TrangThai)
 VALUES ('CUS001', 'Xuan123@gmail.com', 'abc123','Le Thi Xuan', '119/34A Tran Hoan Street District 3 HCMC', '1'),
 		('CUS002', 'Toan123@yahoo.com', 'abc456','Tran Hung Toan', '34Bis Nguyen Van Loi Street District HCMC', '0'),
 		('CUS003', 'Huy123@gmail.com', 'abc789','Le Huy', '18/8H Tran Ke Xuong Street Phu Nhuan District HCMC', '1'),
@@ -92,7 +92,7 @@ VALUES ('CUS001', 'Xuan123@gmail.com', 'abc123','Le Thi Xuan', '119/34A Tran Hoa
 		('CUS005', 'Khanh123@yahoo.com', 'def45', 'Le Viet Khanh', '23B Nguyen Van Nghi Street Go Vap District HCMC', '1')
 GO
 
-INSERT INTO[Order] (CustomerID, Address, Amount, RequireDate, OrderDate, Description, Status)
+INSERT INTO DonHang(IDKhachHang, DiaChi, SoLuong, NgayGiao, NgayDat, MoTa, TrangThai)
 VALUES ('CUS002', '34Bis Nguyen Van Loi Street District 1 HCMC', '2', '03/25/2017', '03/27/2017', '', '1'),
 		('CUS001', '119/34A Tran Hoan Street District 3 HCMC', '3', '04/14/2017', '04/16/2017', '', '0'),
 		('CUS005', '23B Nguyen Van Nghi Street Go Vap District HCMC', '5', '09/25/2017', '09/30/2017', '', '1'),
@@ -100,7 +100,7 @@ VALUES ('CUS002', '34Bis Nguyen Van Loi Street District 1 HCMC', '2', '03/25/201
 		('CUS004', '45/9 Le Loi Street District 1 HCMC', '8', '04/01/2017', '04/04/2017', '', '0')
 GO  
 
-INSERT INTO[OrderDetail] (OrderID, ProductID, Discount, Quantity, UnitPrice)
+INSERT INTO[ChiTietDonHang] (IDDonHang, IDSanPham, GiamGia, SoLuong, DonGia)
 VALUES ('1', '2', '0.1', '10', '14500000'),
 		('2', '1', '0.05', '15', '13800000'),
 		('5', '3', '0.05', '8', '13200000'),
